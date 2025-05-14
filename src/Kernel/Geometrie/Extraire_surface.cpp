997ced784d5741f3476da0d905bfb618764c58c4
Antoine Gerschenfeld <antoine.gerschenfeld@cea.fr>
2025-05-14 14:52:11 +0200

    Extraire_Surface: add option to select by Groupe_Faces

--- src/Kernel/Geometrie/Extraire_surface.cpp
+++ src/Kernel/Geometrie/Extraire_surface.cpp
@@ -37,7 +37,7 @@ Entree& Extraire_surface::interpreter_(Entree& is)
   Nom nom_domaine_surfacique;
   Nom expr_elements("1"),expr_faces("1");
   bool avec_les_bords = false;
-  Noms noms_des_bords;
+  Noms noms_des_bords, groupes_faces;
   Param param(que_suis_je());
   param.ajouter("domaine",&nom_domaine_surfacique,Param::REQUIRED); // XD_ADD_P ref_domaine Domain in which faces are saved
   param.ajouter("probleme",&nom_pb,Param::REQUIRED); // XD_ADD_P ref_Pb_base Problem from which faces should be extracted
@@ -45,6 +45,7 @@ Entree& Extraire_surface::interpreter_(Entree& is)
   param.ajouter("condition_faces",&expr_faces); // XD_ADD_P chaine not_set
   param.ajouter_flag("avec_les_bords",&avec_les_bords); // XD_ADD_P rien not_set
   param.ajouter("avec_certains_bords",&noms_des_bords); // XD_ADD_P listchaine not_set
+  param.ajouter("groupes_faces",&groupes_faces); // XD_ADD_P listchaine not_set
   param.lire_avec_accolades_depuis(is);
 
   associer_domaine(nom_domaine_surfacique);
@@ -69,19 +70,19 @@ Entree& Extraire_surface::interpreter_(Entree& is)
   const Domaine_VF& domaine_vf=ref_cast(Domaine_VF,pb.domaine_dis());
   const Domaine& domaine_volumique = domaine_vf.domaine();
 
-  extraire_surface(domaine_surfacique,domaine_volumique,nom_domaine_surfacique,domaine_vf,expr_elements,expr_faces,avec_les_bords,noms_des_bords);
+  extraire_surface(domaine_surfacique,domaine_volumique,nom_domaine_surfacique,domaine_vf,expr_elements,expr_faces,avec_les_bords,noms_des_bords, groupes_faces);
 
   return is;
 }
 
 // Extraction d'une ou plusieurs frontieres du domaine volumique selon certaines conditions
 
-void Extraire_surface::extraire_surface(Domaine& domaine_surfacique,const Domaine& domaine_volumique, const Nom& nom_domaine_surfacique, const Domaine_VF& domaine_vf, const Nom& expr_elements,const Nom& expr_faces, bool avec_les_bords, const Noms& noms_des_bords)
+void Extraire_surface::extraire_surface(Domaine& domaine_surfacique,const Domaine& domaine_volumique, const Nom& nom_domaine_surfacique, const Domaine_VF& domaine_vf, const Nom& expr_elements,const Nom& expr_faces, bool avec_les_bords, const Noms& noms_des_bords, const Noms& groupes_faces)
 {
-  extraire_surface_without_cleaning(domaine_surfacique,domaine_volumique,nom_domaine_surfacique,domaine_vf,expr_elements,expr_faces,avec_les_bords,noms_des_bords);
+  extraire_surface_without_cleaning(domaine_surfacique,domaine_volumique,nom_domaine_surfacique,domaine_vf,expr_elements,expr_faces,avec_les_bords,noms_des_bords, groupes_faces);
   NettoieNoeuds::nettoie(domaine_surfacique);
 }
-void Extraire_surface::extraire_surface_without_cleaning(Domaine& domaine_surfacique,const Domaine& domaine_volumique, const Nom& nom_domaine_surfacique, const Domaine_VF& domaine_vf, const Nom& expr_elements,const Nom& expr_faces, bool avec_les_bords, const Noms& noms_des_bords)
+void Extraire_surface::extraire_surface_without_cleaning(Domaine& domaine_surfacique,const Domaine& domaine_volumique, const Nom& nom_domaine_surfacique, const Domaine_VF& domaine_vf, const Nom& expr_elements,const Nom& expr_faces, bool avec_les_bords, const Noms& noms_des_bords, const Noms& groupes_faces)
 {
   domaine_surfacique.nommer(nom_domaine_surfacique);
   Parser_U condition_elements,condition_faces;
@@ -235,6 +236,14 @@ void Extraire_surface::extraire_surface_without_cleaning(Domaine& domaine_surfac
   }, nb_t);
   end_gpu_timer(__KERNEL_NAME__);
 
+  for (auto && nom : groupes_faces)
+    {
+      const ArrOfInt& arr = domaine_vf.domaine().groupe_faces(nom).get_indices_faces();
+      for (int i = 0; i < arr.size_array(); i++)
+        if (marq[arr[i]] != 1)
+          marq[arr[i]] = 1, nb_t++;
+    }
+
   ArrOfInt tab_indices(nb_faces);
   IntArrView indices = tab_indices.view_wo();
   Kokkos::parallel_scan(start_gpu_timer(__KERNEL_NAME__), nb_faces, KOKKOS_LAMBDA(const int fac, int& update, const bool final)
@@ -246,6 +255,8 @@ void Extraire_surface::extraire_surface_without_cleaning(Domaine& domaine_surfac
       }
   });
   end_gpu_timer(__KERNEL_NAME__);
+
+
 
   Cerr<<"Number of elements of the new domain : "<<nb_t<<finl;
   CIntTabView face_sommets = domaine_vf.face_sommets().view_ro();
