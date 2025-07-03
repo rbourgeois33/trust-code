dc6876ae8ea9db7bf1c60f2ca5ec174a1aa4d448
Yannick Gorsse <yannick.gorsse@stellaria-energy.com>
2025-07-03 11:38:50 +0200

    [Poly] Minor refactoring to make Poly* schemes compatible with ALE (resize and dimension only once for example)

--- src/Kernel/VF/Geometrie/Domaine_Poly_base.h
+++ src/Kernel/VF/Geometrie/Domaine_Poly_base.h
@@ -135,7 +135,14 @@ public :
   //MD_Vectors pour Champ_Elem_PolyMAC_CDO (elems + faces) et pour Champ_Face_PolyMAC_CDO (faces + aretes)
   mutable MD_Vector mdv_elems_faces, mdv_faces_aretes;
 
+  void calculer_infos_aretes();
+  void fill_normales();
+  void recalculer_xv();
+
 protected:
+  void verifier_type_elem() const;
+  void corriger_face_voisins_sur_les_faces_virtuelles();
+
   double h_carre = DMAXFLOAT;			 // carre du pas du maillage
   DoubleVect h_carre_;			// carre du pas d'une maille
   OWN_PTR(Elem_poly_base) type_elem_;                  // type de l'element de discretisation
