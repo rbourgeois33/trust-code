210c8b5e60ac87b071d7f6b697f15394583b7929
Yannick Gorsse <yannick.gorsse@stellaria-energy.com>
2025-08-13 14:09:27 +0200

    [ALE] Remove `second_special_treatment` ➜ use_old_volumes directly in solveur_masse

--- src/Kernel/Geometrie/Domaine_base.h
+++ src/Kernel/Geometrie/Domaine_base.h
@@ -68,7 +68,7 @@ public:
   int mesh_update_required() const { return mesh_update_required_; }
   virtual const DoubleVect& old_volumes_entrelaces() const { throw; }
   virtual const DoubleVect& old_volumes() const { throw; }
-  virtual void apply_old_to_new_volume_scaling(DoubleTab& tab) { }
+  virtual void apply_old_to_new_volume_scaling(DoubleTab& tab) const { }
   virtual bool getUpdateTheGrid() {return true;}
   virtual void setUpdateTheGrid(bool) {}
 
--- src/ThHyd/Schemas_Temps/Piso.cpp
+++ src/ThHyd/Schemas_Temps/Piso.cpp
@@ -171,9 +171,6 @@ void Piso::iterer_NS(Equation_base& eqn,DoubleTab& current,DoubleTab& pression,
 
   le_solveur_->reinit();
 
-  //sometimes we need a second special treatement like for ALE for example
-  second_special_treatment( eqn, current, resu, matrice );
-
   //Construction de matrice_en_pression_2 = BD-1Bt[Un]
   //Assemblage reeffectue seulement pour algorithme Piso (avancement_crank_==0)
   Matrice& matrice_en_pression_2 = eqnNS.matrice_pression();
@@ -449,11 +446,6 @@ void Piso::iterer_NS_PolyMAC(Navier_Stokes_std& eqn, DoubleTab& current, DoubleT
   current = v_new;
 }
 
-void Piso::second_special_treatment(Equation_base& eqn,DoubleTab& current, DoubleTrav& resu, Matrice_Morse& matrice)
-{
-  //nothing to do
-}
-
 void Piso::add_penality_term(Navier_Stokes_std& eqnNS, DoubleTrav& resu , DoubleTrav& gradP)
 {
   // <IBM> Taking into account penality term for Immersed Boundary Method
@@ -536,10 +528,3 @@ void Piso::correct_pressure(Navier_Stokes_std& eqnNS, DoubleTab& pression, Doubl
   else
     pression += correction_en_pression;
 }
-
-void Implicite::second_special_treatment(Equation_base& eqn,DoubleTab& current, DoubleTrav& resu, Matrice_Morse& matrice)
-{
-  //nothing to do
-}
-
-
