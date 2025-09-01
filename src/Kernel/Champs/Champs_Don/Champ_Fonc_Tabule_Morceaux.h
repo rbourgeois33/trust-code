5d23bb912c66df6a26ac50f766187bfb081e4f3d
Yannick Gorsse <yannick.gorsse@stellaria-energy.com>
2025-09-01 13:45:51 +0200

    [Kernel] Allow Champ_Fonc_Tabule_Morceaux to use post-processed fields

--- src/Kernel/Champs/Champs_Don/Champ_Fonc_Tabule_Morceaux.h
+++ src/Kernel/Champs/Champs_Don/Champ_Fonc_Tabule_Morceaux.h
@@ -1,5 +1,5 @@
 /****************************************************************************
-* Copyright (c) 2023, CEA
+* Copyright (c) 2025, CEA
 * All rights reserved.
 *
 * Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
@@ -52,6 +52,7 @@ protected :
   } CHTAB;
   std::vector<CHTAB> morceaux; /* les morceaux */
   IntVect i_mor; //i_mor[e] : morceau de l'element e
+  OWN_PTR(Champ_base) espace_stockage_;
 };
 
 #endif /* Champ_Fonc_Tabule_Morceaux_included */
