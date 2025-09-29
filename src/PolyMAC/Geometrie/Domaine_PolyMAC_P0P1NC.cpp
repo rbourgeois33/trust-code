afd51d70b818a2a7f4448b86e6bece161779ff72
Yannick Gorsse <yannick.gorsse@stellaria-energy.com>
2025-09-29 13:58:21 +0200

    [PolyMAC_P0] Fixes for bidim_axi

--- src/PolyMAC/Geometrie/Domaine_PolyMAC_P0P1NC.cpp
+++ src/PolyMAC/Geometrie/Domaine_PolyMAC_P0P1NC.cpp
@@ -64,10 +64,35 @@ void Domaine_PolyMAC_P0P1NC::discretiser()
 
 void Domaine_PolyMAC_P0P1NC::calculer_volumes_entrelaces()
 {
+  const double tol_r = 1e-12;
+  const IntTab& fsom = face_sommets();
+  const DoubleTab& xs = domaine().coord_sommets();
+
   //volumes_entrelaces_ et volumes_entrelaces_dir : par projection de l'amont/aval sur la normale a la face
-  for (int f = 0, i, e; f < nb_faces(); f++)
-    for (i = 0; i < 2 && (e = face_voisins_(f, i)) >= 0; volumes_entrelaces_(f) += volumes_entrelaces_dir_(f, i), i++)
-      volumes_entrelaces_dir_(f, i) = std::fabs(dot(&xp_(e, 0), &face_normales_(f, 0), &xv_(f, 0)));
+  for (int f = 0, e; f < nb_faces(); f++)
+    {
+      const bool axis_face = bidim_axi && (face_voisins_(f, 0) < 0 || face_voisins_(f, 1) < 0) && std::fabs(xv_(f, 0)) <= tol_r;
+      double axis_length = 0.;
+
+      if (axis_face)
+        {
+          assert(fsom.dimension(1) == 2);
+          const int s0 = fsom(f, 0);
+          const int s1 = fsom(f, 1);
+          const double dr = xs(s1, 0) - xs(s0, 0);
+          const double dz = xs(s1, 1) - xs(s0, 1);
+          axis_length = std::sqrt(dr * dr + dz * dz);
+        }
+
+      for (int i = 0; i < 2 && (e = face_voisins_(f, i)) >= 0; i++)
+        {
+          volumes_entrelaces_dir_(f, i) = axis_face
+                                          ? volume_entrelace_axi(std::fabs(xv_(f, 0)), std::fabs(xp_(e, 0)), axis_length)
+                                          : std::fabs(dot(&xp_(e, 0), &face_normales_(f, 0), &xv_(f, 0)));
+
+          volumes_entrelaces_(f) += volumes_entrelaces_dir_(f, i);
+        }
+    }
   volumes_entrelaces_.echange_espace_virtuel(), volumes_entrelaces_dir_.echange_espace_virtuel();
 }
 
