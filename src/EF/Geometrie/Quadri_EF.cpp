ff662b2f010f5b1c99b64ace9561685d4ad3c5af
Yannick Gorsse <yannick.gorsse@stellaria-energy.com>
2025-10-06 09:22:40 +0200

    [EF] Enable bidim_axi support and add volumetric viscosity handling in Op_Diff_EF and related geometry classes

--- src/EF/Geometrie/Quadri_EF.cpp
+++ src/EF/Geometrie/Quadri_EF.cpp
@@ -1,5 +1,5 @@
 /****************************************************************************
-* Copyright (c) 2024, CEA
+* Copyright (c) 2025, CEA
 * All rights reserved.
 *
 * Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
@@ -120,8 +120,16 @@ void Quadri_EF::normale(int num_Face,DoubleTab& Face_normales,
       if(psc<0)
         sign=-1;
     }
-  Face_normales(num_Face,0)=sign*nx;
-  Face_normales(num_Face,1)=sign*ny;
+  double scale = 1.0;
+  if (bidim_axi)
+    {
+      const double r0 = les_coords(n0, 0);
+      const double r1 = les_coords(n1, 0);
+      const double r_bar = 0.5 * (r0 + r1);
+      scale = 2.0 * M_PI * ((r_bar <=1e-10) ? x1g / 8.0 : r_bar);
+    }
+  Face_normales(num_Face, 0) = sign * nx * scale;
+  Face_normales(num_Face, 1) = sign * ny * scale;
 }
 
 /*! @brief
@@ -400,4 +408,3 @@ void Quadri_EF::calcul_xg(DoubleVect& xg, const DoubleTab& x,
 //   } // fin du switch
 
 }
-
