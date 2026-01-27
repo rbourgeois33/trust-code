11f1bf2c4749fea525fdbda4466b1eed32109912
Yannick Gorsse <yannick.gorsse@stellaria-energy.com>
2026-01-27 11:04:17 +0100

    Fix champ_fonc_reprise with polyvef_P0P1

--- src/PolyMAC/Champs/Champ_Elem_PolyVEF_P0P1.cpp
+++ src/PolyMAC/Champs/Champ_Elem_PolyVEF_P0P1.cpp
@@ -1,5 +1,5 @@
 /****************************************************************************
-* Copyright (c) 2025, CEA
+* Copyright (c) 2026, CEA
 * All rights reserved.
 *
 * Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
@@ -100,7 +100,7 @@ const DoubleTab& Champ_Elem_PolyVEF_P0P1::alpha_es() const
 DoubleTab& Champ_Elem_PolyVEF_P0P1::valeur_aux_elems(const DoubleTab& positions, const IntVect& polys, DoubleTab& result) const
 {
   const Domaine_PolyMAC& dom = ref_cast(Domaine_PolyVEF_P0P1,le_dom_VF.valeur());
-  if (!sub_type(Navier_Stokes_std, equation()) || (this != &ref_cast(Navier_Stokes_std, equation()).pression() && this != &ref_cast(Navier_Stokes_std, equation()).pression_pa()) || valeurs().dimension_tot(0) == dom.nb_elem_tot())
+  if (mon_equation.est_nul() || !sub_type(Navier_Stokes_std, equation()) || (this != &ref_cast(Navier_Stokes_std, equation()).pression() && this != &ref_cast(Navier_Stokes_std, equation()).pression_pa()) || valeurs().dimension_tot(0) == dom.nb_elem_tot())
     return Champ_Elem_PolyMAC_P0::valeur_aux_elems(positions, polys, result); //ne s'applique qu'a la pression
   const IntTab& e_s = dom.domaine().les_elems();
   const DoubleTab& vals = valeurs(), &a_es = alpha_es();
